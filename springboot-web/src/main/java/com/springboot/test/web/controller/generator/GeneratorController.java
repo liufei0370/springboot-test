1c6dd5a31ba3a1ad2354d8c676b6c15c746b14f2
liufei1 <1qaz!QAZ>
2019-05-29 10:29:48 +0800

    1.controller日期格式通用配置

---
+++ springboot-web/src/main/java/com/springboot/test/web/controller/generator/GeneratorController.java
@@ -0,0 +1,50 @@
+package com.springboot.test.web.controller.generator;
+
+import com.springboot.test.beans.User;
+import com.springboot.test.web.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author liufei
+ * @date 2019/5/17 17:39
+ */
+@RequestMapping(value = "/generator/")
+@Controller
+public class GeneratorController extends BaseController {
+
+    @ResponseBody
+    @RequestMapping(value = "test",method = {RequestMethod.POST,RequestMethod.DELETE})
+    public void test(String name,long phone){
+        System.out.println("test的POST方法测试"+name+",phone:"+phone);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "test1",method = RequestMethod.POST)
+    public void test1(@RequestParam String name){
+        System.out.println("test1的POST方法测试"+name);
+    }
+
+    @ResponseBody
+    @PostMapping(value = {"test2"})
+    public void test2(@RequestBody User user){
+        System.out.println("test2POST方法测试"+user.getNickname());
+    }
+
+
+    @ResponseBody
+    @GetMapping(value = "test3/{name}")
+    public void test3(@PathVariable String name){
+        System.out.println("test3的POST方法测试"+name);
+    }
+
+    public void test4(String name){
+        System.out.println("test4的POST方法测试"+name);
+    }
+
+    @ResponseBody
+    @PostMapping(value = {"test5"})
+    public void test5(User user){
+        System.out.println("test5POST方法测试"+user.getNickname());
+    }
+}
